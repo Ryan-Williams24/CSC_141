0783fef04f05af31efb051f465bbb181f6367333
Ryan-Williams24 <ryan.williams001@albright.edu>
2024-09-04 09:16:52 -0400

    work

---
+++ 02_variables/variables.py
@@ -0,0 +1,9 @@
+# Ryan Williams
+# 9-4-24
+# Personalized introduction
+
+name = input("Hello, what is your name? ")
+age = input("Okay " + name + ", now what is your age? ")
+major = input("Now tell me your major ")
+
+print("Hi, my name is " + name + " and I am " + age + " years old! At Albright College, I study " + major + ".")
