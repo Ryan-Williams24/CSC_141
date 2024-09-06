cda548fdc0c2656dccd1696ea2a4c48333e6d6fd
Ryan-Williams24 <ryan.williams001@albright.edu>
2024-09-05 21:14:44 -0400

    cleaning up

--- 02_variables/name_cases.py
+++
@@ -1,9 +0,0 @@
-name = "Eric"
-n=0
-print("Hello " + name + ", would you like to learn some python today?")
-
-while (n!=3):
-    print(name)
-    n = n+1
-inpu = input("hello what is your name")
-print(inpu)
