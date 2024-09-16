d51ae2449adfbb695af1500595b0c859158b4db8
Ryan-Williams24 <ryan.williams001@albright.edu>
2024-09-16 09:46:15 -0400

    finishing chapter 3

---
+++ Chapter_3/3_7_Shrinking_Guest_List.py
@@ -0,0 +1,31 @@
+invitees = ["Beyonce", "Sydney Sweeney", "Cassie Howard"]
+removed = []
+
+for names in invitees: 
+    print(names + ", you are invited to dinner!")
+
+print("\n" + invitees[2] + " could not make it and will be replaced with Tokyo Toni\n")
+
+invitees[2] = "Tokyo Toni"
+
+for names in invitees:
+    print(names + ", you are invited to dinner!")
+
+print("\nA bigger table has been acquired and more people can be seated.")
+
+invitees.insert(0, "Gerald")
+
+invitees.insert(2, "Deni")
+
+invitees.append("Kamala Harris")
+
+for names in invitees:
+    print(names + ", you are invited to dinner!")
+
+print("I can only invite two people for dinner")
+
+for i in invitees: 
+    if removed < [4] :
+        removed.append(i)
+        invitees.pop([i])
+        print(i + ", you have been removed.")
